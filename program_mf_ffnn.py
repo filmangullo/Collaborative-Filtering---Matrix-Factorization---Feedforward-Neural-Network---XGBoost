4bba87da5b634bfa2204a6f8124db3e2f0e53ee3
filmangullo <filmantapgullo@gmail.com>
2025-12-09 13:41:38 +0700

    make tunning hyperpameter to dataset film

--- program_mf_ffnn.py
+++ program_mf_ffnn.py
@@ -128,9 +128,9 @@ print(f"\n")
 # 4. Tuning Hyperparameter Matrix Factorization
 # ---------------------------------------------
 k = 64            # latent factors
-alpha = 0.002      # learning rate
-beta = 0.05       # regularization parameter
-epochs_mf = 40    # early stopping
+alpha = 0.03      # learning rate
+beta = 0.03       # regularization parameter
+epochs_mf = 35    # early stopping
 
 print("Hyperparameter Matrix Factorization:")
 print(f"Latent factors / Dimensi laten: {k}")
@@ -174,7 +174,7 @@ gc.collect()
 hidden_layer=[128, 64, 32, 16]  #Struktur jaringan (jumlah layer) dengan value adalah Jumlah Neuron
 learning_rate=0.005          #Kecepatan pembelajaran
 batch_size=64                #Jumlah data per batch
-epochs_mlp=55                #Total maksimum iterasi
+epochs_mlp=50                #Total maksimum iterasi
 patience=15                  #Toleransi stagnasi saat training
 
 print("Hyperparameter MLP:")
